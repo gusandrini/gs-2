08e6a9fa9cce01143ee09f059a49f482060915c1
gusandrini <gustavosandrini92@gmail.com>
2024-11-06 12:16:45 -0300

    estruruta rodapé

--- gs/src/components/Rodape/page.tsx
+++ gs/src/components/Rodape/page.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import Link from 'next/link';
 
-export const Rodape = () => {
+const Rodape = () => {
   return (
-    <div>Rodape</div>
+    <div>
+        <footer>
+            <div className="parceiros">
+                <h2 className="titulo_rodape"></h2>
+                <ul>
+                    <li><Link className='' href='/'></Link></li>
+                    <li><Link className='' href='/'></Link></li>
+                    <li><Link className='' href='/'></Link></li>
+                </ul>
+            </div>
+
+            <div className="redes_sociais">
+                <div className="parceiros">
+                    <h2 className="titulo_rodape"></h2>
+                    <ul>
+                        <li><Link className='' href='/'></Link></li>
+                        <li><Link className='' href='/'></Link></li>
+                        <li><Link className='' href='/'></Link></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
   )
 }
+export default Rodape;
