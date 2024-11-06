ac0eba2de3bb1762428c15f83969d66b131f8450
gusandrini <gustavosandrini92@gmail.com>
2024-11-06 17:03:49 -0300

    adicionando imagem de fundo

--- gs/src/app/page.tsx
+++ gs/src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Inicial from './conteudo-inicial/page';
+import Paisagem from './paisagem/page';
 
 const Home = () => {
   return (
     <div>
+      <Paisagem/>
       <Inicial/>
     </div>
   )
---
+++ gs/src/app/paisagem/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Eolica from '@/Image/eolica.jpg';
+
+const Paisagem = () => {
+  return (
+    <div
+      className="relative h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${Eolica.src})`
+      }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div> {/* Overlay de transparência */}
+      
+      <div className="relative z-10 flex justify-center items-center h-full text-white">
+        <h1 className="text-4xl font-bold">Energia Eólica</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Paisagem;
