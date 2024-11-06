acba800c21353a537fa72ef72624e6e06dff40c1
gusandrini <gustavosandrini92@gmail.com>
2024-11-06 17:23:54 -0300

    ajustando imagens

--- gs/src/components/Rodape/page.tsx
+++ gs/src/components/Rodape/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Face from '@/Image/facebook.png';
 import Insta from '@/Image/instagram.png';
+import GitHub from '@/Image/github.png';
 
 const Rodape = () => {
   return (
@@ -36,6 +37,16 @@ const Rodape = () => {
               <li>
                 <Link className='instagram' target='_blank' rel='noopener' title='site Instagram' href="/">
                   <Image src={Insta} alt='Instagram' />
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="git">
+            <ul>
+              <li>
+                <Link className='GitHub' target='_blank' rel='noopener' title='site GitHub' href="/">
+                  <Image src={GitHub} alt='IGitHub' />
                 </Link>
               </li>
             </ul>
