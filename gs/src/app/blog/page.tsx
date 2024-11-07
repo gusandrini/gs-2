ca752340f398be82e46b9bea37c8334c8fff0cd5
gusandrini <gustavosandrini92@gmail.com>
2024-11-07 01:43:04 -0300

    ajuste na estilização da página do blog

--- gs/src/app/blog/page.tsx
+++ gs/src/app/blog/page.tsx
@@ -1,107 +1,80 @@
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 const Blog = () => {
   return (
     <div>
       <div className="paginas">
-        <Link href="/">Home/Blog</Link>
+        <Link href="/" passHref>Home/Blog</Link>
       </div>
 
       <div className="introducao_blog">
-        <h1>
-          Energia sustentável em Casa: Como Começar e Quanto Você Pode Economizar
-        </h1>
-        <h2>
-        Por que a Energia Sustentável é o Futuro da Sustentabilidade? *usaremos de exemplo a energia solar*
-        </h2>
+        <h1>Energia sustentável em Casa: Como Começar e Quanto Você Pode Economizar</h1>
+        <h2>Por que a Energia Sustentável é o Futuro da Sustentabilidade? <em>Usaremos de exemplo a energia solar</em></h2>
         <p>
-          A energia solar não é apenas uma tendência passageira; ela está se tornando uma das formas mais acessíveis e eficazes de obter energia limpa em casa. Cada vez mais pessoas estão optando por painéis solares para reduzir suas contas de eletricidade, diminuir a pegada de carbono e contribuir para um futuro mais verde. Mas, por onde começar? Neste artigo, vamos abordar tudo o que você precisa saber para entender o processo de instalação, os benefícios e as economias que você pode obter. <br />
-          <br />
+          A energia solar não é apenas uma tendência passageira; ela está se tornando uma das formas mais acessíveis e eficazes de obter energia limpa em casa. Cada vez mais pessoas estão optando por painéis solares para reduzir suas contas de eletricidade, diminuir a pegada de carbono e contribuir para um futuro mais verde. Mas, por onde começar? Neste artigo, vamos abordar tudo o que você precisa saber para entender o processo de instalação, os benefícios e as economias que você pode obter.
         </p>
       </div>
 
       <div className="guia_rapido">
-        <h1>
-          Como funciona? Guia rápido
-        </h1>
+        <h1>Como funciona? Guia rápido</h1>
         <p>
           Antes de explorar o processo de instalação, é importante entender como a energia solar funciona. Em resumo, os painéis solares captam a luz do sol e a convertem em eletricidade por meio de células fotovoltaicas. Essa energia pode ser usada em sua casa, e o excedente pode ser armazenado em baterias ou vendido de volta à rede elétrica, dependendo da sua localização e regulamentações locais.
         </p>
         <h2>Componentes do Sistema Solar Residencial:</h2>
         <p>
-          Painéis Solares: Captam a luz e a transformam em energia. <br />
-          Inversores: Convertem a energia para um formato utilizável em casa. <br />
-          Baterias (opcional): Armazenam a energia extra para dias nublados ou uso noturno. <br />
-          <br />
+          Painéis Solares: <br /> Captam a luz e a transformam em energia.<br />
+          Inversores: <br /> Convertem a energia para um formato utilizável em casa.<br />
+          Baterias (opcional): <br /> Armazenam a energia extra para dias nublados ou uso noturno.
         </p>
       </div>
 
       <div className="beneficios">
-        <h1>
-          Benefícios da Energia Solar em Casa
-        </h1>
+        <h1>Benefícios da Energia Solar em Casa</h1>
         <p>
-          Economia nas Contas de Energia: Dependendo do consumo da sua casa, é possível economizar de 30% a 80% na conta de luz. Em algumas regiões, a economia pode ser ainda maior. <br />
-          Valorização do Imóvel: Casas com sistemas de energia renovável são mais atraentes para compradores e podem ter um valor de mercado superior. <br />
-          Redução da Pegada de Carbono: Ao reduzir a dependência de combustíveis fósseis, você está contribuindo para um futuro mais sustentável e reduzindo sua própria pegada de carbono. <br />
-          <br />
+          Economia nas Contas de Energia: <br /> Dependendo do consumo da sua casa, é possível economizar de 30% a 80% na conta de luz. Em algumas regiões, a economia pode ser ainda maior.<br />
+          Valorização do Imóvel: <br /> Casas com sistemas de energia renovável são mais atraentes para compradores e podem ter um valor de mercado superior.<br />
+          Redução da Pegada de Carbono: <br /> Ao reduzir a dependência de combustíveis fósseis, você está contribuindo para um futuro mais sustentável e reduzindo sua própria pegada de carbono.
         </p>
       </div>
 
       <div className="como_comecar">
-        <h1>
-        Como Começar? Passo a Passo para Instalar Energia Solar em Casa
-        </h1>
+        <h1>Como Começar? Passo a Passo para Instalar Energia Solar em Casa</h1>
         <p>
-          Passo 1: Avalie seu Consumo de Energia <br />
-          Antes de instalar os painéis, analise o quanto de energia sua casa consome por mês. Isso ajudará a determinar o tamanho do sistema necessário. <br />
-          Passo 2: Consulte um Profissional <br />
-          Contratar uma empresa especializada para realizar uma análise de viabilidade é essencial. Eles podem avaliar a melhor posição para os painéis, calcular a quantidade de luz solar que sua casa recebe e estimar o custo total. <br />
-          Passo 3: Escolha o Tipo de Sistema <br />
-          Existem diferentes tipos de sistemas solares, como on-grid (conectado à rede elétrica) e off-grid (independente da rede). Cada tipo tem vantagens específicas, dependendo da sua localização e das suas necessidades. <br />
-          Passo 4: Instalação e Manutenção <br />
-          A instalação é geralmente rápida e feita em poucos dias. Após a instalação, a manutenção é mínima. Limpar os painéis uma ou duas vezes por ano e monitorar o sistema são os principais cuidados necessários. <br />
-          <br />
+          Passo 1: <br />Avalie seu Consumo de Energia. Antes de instalar os painéis, analise o quanto de energia sua casa consome por mês. Isso ajudará a determinar o tamanho do sistema necessário.<br />
+          Passo 2: <br /> Consulte um Profissional. Contratar uma empresa especializada para realizar uma análise de viabilidade é essencial. Eles podem avaliar a melhor posição para os painéis, calcular a quantidade de luz solar que sua casa recebe e estimar o custo total.<br />
+          Passo 3: <br /> Escolha o Tipo de Sistema. Existem diferentes tipos de sistemas solares, como on-grid (conectado à rede elétrica) e off-grid (independente da rede). Cada tipo tem vantagens específicas, dependendo da sua localização e das suas necessidades.<br />
+          Passo 4: <br /> Instalação e Manutenção. A instalação é geralmente rápida e feita em poucos dias. Após a instalação, a manutenção é mínima. Limpar os painéis uma ou duas vezes por ano e monitorar o sistema são os principais cuidados necessários.
         </p>
       </div>
 
       <div className="economia">
-        <h1>
-        Quanto Você Pode Economizar com Energia Solar?
-        </h1>
+        <h1>Quanto Você Pode Economizar com Energia Solar?</h1>
         <p>
-        A economia gerada pela energia solar varia de acordo com fatores como consumo de energia, tamanho do sistema e quantidade de luz solar na sua região. Veja um exemplo prático: <br />
-        Exemplo de Economia: Suponha que você consome, em média, R$ 400 de eletricidade por mês. Com um sistema de energia solar de tamanho médio, você poderia reduzir essa conta para cerca de R$ 100 ou menos, dependendo da estação do ano e da eficiência do sistema. br
-        <br />
-        Retorno sobre o Investimento (ROI): Em média, um sistema de energia solar residencial paga-se em 5 a 10 anos, com uma vida útil de cerca de 25 anos. Isso significa que você desfrutará de energia gratuita por muitos anos após o pagamento inicial. <br />
-        <br />
+          A economia gerada pela energia solar varia de acordo com fatores como consumo de energia, tamanho do sistema e quantidade de luz solar na sua região. Veja um exemplo prático:<br />
+          Exemplo de Economia: <br /> Suponha que você consome, em média, R$ 400 de eletricidade por mês. Com um sistema de energia solar de tamanho médio, você poderia reduzir essa conta para cerca de R$ 100 ou menos, dependendo da estação do ano e da eficiência do sistema.<br />
+          Retorno sobre o Investimento (ROI): <br /> Em média, um sistema de energia solar residencial paga-se em 5 a 10 anos, com uma vida útil de cerca de 25 anos. Isso significa que você desfrutará de energia gratuita por muitos anos após o pagamento inicial.
         </p>
       </div>
 
       <div className="dicas">
-        <h1>
-        Dicas para Maximizar a Eficiência do Seu Sistema Solar
-        </h1>
+        <h1>Dicas para Maximizar a Eficiência do Seu Sistema Solar</h1>
         <h2>Aqui estão algumas dicas simples para garantir que o seu sistema solar funcione da melhor maneira:</h2>
         <p>
-        Posicionamento Adequado: Certifique-se de que os painéis estão voltados para a direção que recebe mais luz ao longo do dia. <br />
-        Manutenção Regular: Mantenha os painéis limpos e verifique o sistema uma vez ao ano para garantir a máxima eficiência. <br />
-        Monitore seu Consumo: Muitos sistemas vêm com aplicativos que monitoram a produção de energia. Isso ajuda a entender melhor seu consumo e otimizar o uso. <br />
-        <br />
+          Posicionamento Adequado: <br /> Certifique-se de que os painéis estão voltados para a direção que recebe mais luz ao longo do dia.<br />
+          Manutenção Regular: <br /> Mantenha os painéis limpos e verifique o sistema uma vez ao ano para garantir a máxima eficiência.<br />
+          Monitore seu Consumo: <br /> Muitos sistemas vêm com aplicativos que monitoram a produção de energia. Isso ajuda a entender melhor seu consumo e otimizar o uso.
         </p>
       </div>
 
       <div className="incentivos">
-        <h1>
-        Energia Solar e Incentivos Governamentais
-        </h1>
+        <h1>Energia Solar e Incentivos Governamentais</h1>
         <p>
-        Em algumas regiões, existem incentivos para a instalação de sistemas solares, como redução de impostos, financiamento acessível e créditos de energia. Esses incentivos podem variar, então vale a pena pesquisar e verificar se sua cidade ou estado oferece algum tipo de benefício.
+          Em algumas regiões, existem incentivos para a instalação de sistemas solares, como redução de impostos, financiamento acessível e créditos de energia. Esses incentivos podem variar, então vale a pena pesquisar e verificar se sua cidade ou estado oferece algum tipo de benefício.
         </p>
       </div>
-
     </div>
-  )
-}
+  );
+};
+
 export default Blog;
