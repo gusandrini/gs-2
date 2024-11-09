a5435a2de819db1b08baee8885a085f78489a2c6
gusandrini <gustavosandrini92@gmail.com>
2024-11-09 18:34:58 -0300

    ajuste na pagina sobre nos

--- gs/src/app/sobre/page.tsx
+++ gs/src/app/sobre/page.tsx
@@ -40,22 +40,46 @@ const Sobre = () => {
         <div className="nossa_equipe_titulo">
           <h1>Equipe</h1>
         </div>
+
         <div className="nossa_equipe">
+          <div className="duda">
+              <Image alt="Imagem de Eduarda" src={Duda} />
+              <p>Eduarda Tiemi</p>
+              <p>
+                  <Link href="https://github.com/dudatiemiak">GitHub</Link>
+              </p>
+              <p>
+                  <Link href="">LinkedIn</Link>
+              </p>
+              <p>Turma: 1TDSPH</p>
+              <p>RM: 554756</p>
+          </div>
 
-            <div className="duda">
-                <Image alt='Imagem de Eduarda' src={Duda}/>
-                <h2>Eduarda Tiemi <br /> RM:XXXXXX</h2>
-            </div>
+          <div className="gu">
+              <Image alt="Imagem de Gustavo" src={Gu} />
+              <p>Gustavo Sandrini</p>
+              <p>
+                  <Link href="https://github.com/gusandrini">GitHub</Link>
+              </p>
+              <p>
+                  <Link href="https://www.linkedin.com/in/gustavo-oliveira-7311ba2ab/">LinkedIn</Link>
+              </p>
+              <p>Turma: 1TDSPH</p>
+              <p>RM: 557505</p>
+          </div>
 
-            <div className="gu">
-                <Image alt='Imagem de Gustavo' src={Gu}/>
-                <h2>Gustavo Sandrini <br /> RM:XXXXXX</h2>
-            </div>
-
-            <div className="vi">
-                <Image alt='Imagem de Victor' src={Vi}/>
-                <h2>Victor Agrela <br /> RM:XXXXXX</h2>
-            </div>
+          <div className="vi">
+              <Image alt="Imagem de Victor" src={Vi} />
+              <p>Victor Agrela</p>
+              <p>
+                  <Link href="https://github.com/victoragrela">GitHub</Link>
+              </p>
+              <p>
+                  <Link href="">LinkedIn</Link>
+              </p>
+              <p>Turma: 1TDSPH</p>
+              <p>RM: 557345</p>
+          </div>
         </div>
 
         <div className="parceirias_colaboracoes">
