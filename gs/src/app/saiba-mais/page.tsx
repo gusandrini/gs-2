0f163bccc561f1358f87253685b17723140edef3
gusandrini <gustavosandrini92@gmail.com>
2024-11-20 20:20:24 -0300

    correção de codigo definido e não usado

--- gs/src/app/saiba-mais/page.tsx
+++ gs/src/app/saiba-mais/page.tsx
@@ -2,7 +2,7 @@
 import { TipoEnergia } from '@/types/types';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const Saiba = () => {
   const [mensagemSaiba, setMensagemSaiba] = useState('');
